17196b7d1ed19140f1fd5006e10d0688385ad559
tevon <tevon.eyabi@student.kuleuven.be>
2025-02-11 00:03:33 +0100

    made insert impl for naive BST

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,65 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+trait BinairyTree<T: PartialOrd + PartialEq> {
+    fn insert(&mut self, item: T);
+    fn search(&self, item: &T) -> Option<usize>;
+    fn delete(&mut self, item: &T) -> Result<(), ()>;
+}
+
+struct BST<T: PartialOrd + PartialEq> {
+    head: Cell<T>,
+}
+#[derive(PartialEq)]
+enum Cell<T: PartialOrd + PartialEq> {
+    Cons {
+        val: T,
+        left: Box<Cell<T>>,
+        right: Box<Cell<T>>,
+    },
+    Nil,
+}
+impl<T: PartialOrd + PartialEq> Cell<T> {
+    fn new_with(val: T) -> Self {
+        Cell::Cons {
+            val,
+            left: Box::new(Cell::Nil),
+            right: Box::new(Cell::Nil),
+        }
+    }
+    fn insert(&mut self, val2: T) {
+        match self {
+            Self::Nil => *self = Self::new_with(val2),
+            Self::Cons { val, left, right } => {
+                if &val2 < val {
+                    left.insert(val2);
+                } else {
+                    right.insert(val2);
+                }
+            }
+        }
+    }
+}
+impl<T: PartialOrd + PartialEq> BST<T> {
+    pub fn new() -> Self {
+        BST { head: Cell::Nil }
+    }
+    pub fn new_with(val: T) -> Self {
+        BST {
+            head: Cell::new_with(val),
+        }
+    }
+}
+impl<T> BinairyTree<T> for BST<T>
+where
+    T: PartialEq + PartialOrd,
+{
+    fn insert(&mut self, item: T) {
+        self.head.insert(item);
+    }
+    fn search(&self, item: &T) -> Option<usize> {
+        todo!();
+    }
+    fn delete(&mut self, item: &T) -> Result<(), ()> {
+        todo!();
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +67,39 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn can_create() {
+        let bst: BST<u32> = BST::new();
+        assert!(bst.head == Cell::Nil);
+        let cell = Cell::new_with(3);
+        assert!(
+            cell == Cell::Cons {
+                val: 3,
+                left: Box::new(Cell::Nil),
+                right: Box::new(Cell::Nil),
+            }
+        );
+    }
+    #[test]
+    fn can_insert() {
+        let mut bst: BST<u32> = BST::new();
+        assert!(bst.head == Cell::Nil);
+        bst.insert(2);
+        assert!(
+            bst.head
+                == Cell::Cons {
+                    val: 2,
+                    left: Box::new(Cell::Nil),
+                    right: Box::new(Cell::Nil),
+                }
+        );
+        bst.insert(1);
+        assert!(
+            bst.head
+                == Cell::Cons {
+                    val: 2,
+                    left: Box::new(Cell::new_with(1)),
+                    right: Box::new(Cell::Nil),
+                }
+        );
     }
 }
