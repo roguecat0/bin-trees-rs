1497f634082d4708ca6a1f4fc22c4c588bc539a1
tevon <tevon.eyabi@student.kuleuven.be>
2025-02-11 21:04:52 +0100

    made search impl and tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,50 +1,28 @@
 trait BinairyTree<T: PartialOrd + PartialEq> {
     fn insert(&mut self, item: T);
-    fn search(&self, item: &T) -> Option<usize>;
+    fn search(&self, item: &T) -> &Self;
     fn delete(&mut self, item: &T) -> Result<(), ()>;
 }
 
-struct BST<T: PartialOrd + PartialEq> {
-    head: Cell<T>,
-}
 #[derive(PartialEq)]
-enum Cell<T: PartialOrd + PartialEq> {
+enum BST<T: PartialOrd + PartialEq> {
     Cons {
         val: T,
-        left: Box<Cell<T>>,
-        right: Box<Cell<T>>,
+        left: Box<BST<T>>,
+        right: Box<BST<T>>,
     },
     Nil,
 }
-impl<T: PartialOrd + PartialEq> Cell<T> {
-    fn new_with(val: T) -> Self {
-        Cell::Cons {
-            val,
-            left: Box::new(Cell::Nil),
-            right: Box::new(Cell::Nil),
-        }
-    }
-    fn insert(&mut self, val2: T) {
-        match self {
-            Self::Nil => *self = Self::new_with(val2),
-            Self::Cons { val, left, right } => {
-                if &val2 < val {
-                    left.insert(val2);
-                } else {
-                    right.insert(val2);
-                }
-            }
-        }
-    }
-}
 impl<T: PartialOrd + PartialEq> BST<T> {
-    pub fn new() -> Self {
-        BST { head: Cell::Nil }
-    }
-    pub fn new_with(val: T) -> Self {
-        BST {
-            head: Cell::new_with(val),
+    fn new_with(val: T) -> Self {
+        Self::Cons {
+            val,
+            left: Box::new(Self::Nil),
+            right: Box::new(Self::Nil),
         }
+    }
+    fn new() -> Self {
+        Self::Nil
     }
 }
 impl<T> BinairyTree<T> for BST<T>
@@ -52,10 +30,29 @@ where
     T: PartialEq + PartialOrd,
 {
     fn insert(&mut self, item: T) {
-        self.head.insert(item);
+        match self {
+            Self::Nil => *self = Self::new_with(item),
+            Self::Cons { val, left, right } => {
+                if &item < val {
+                    left.insert(item);
+                } else if &item > val {
+                    right.insert(item);
+                }
+            }
+        }
     }
-    fn search(&self, item: &T) -> Option<usize> {
-        todo!();
+    fn search(&self, item: &T) -> &Self {
+        match self {
+            Self::Nil => self,
+            Self::Cons { val, .. } if val == item => self,
+            Self::Cons { val, left, right } => {
+                if item < val {
+                    left.search(item)
+                } else {
+                    right.search(item)
+                }
+            }
+        }
     }
     fn delete(&mut self, item: &T) -> Result<(), ()> {
         todo!();
@@ -69,37 +66,57 @@ mod tests {
     #[test]
     fn can_create() {
         let bst: BST<u32> = BST::new();
-        assert!(bst.head == Cell::Nil);
-        let cell = Cell::new_with(3);
+        assert!(bst == BST::Nil);
+        let cell = BST::new_with(3);
         assert!(
-            cell == Cell::Cons {
+            cell == BST::Cons {
                 val: 3,
-                left: Box::new(Cell::Nil),
-                right: Box::new(Cell::Nil),
+                left: Box::new(BST::Nil),
+                right: Box::new(BST::Nil),
             }
         );
     }
     #[test]
     fn can_insert() {
         let mut bst: BST<u32> = BST::new();
-        assert!(bst.head == Cell::Nil);
+        assert!(bst == BST::Nil);
         bst.insert(2);
         assert!(
-            bst.head
-                == Cell::Cons {
-                    val: 2,
-                    left: Box::new(Cell::Nil),
-                    right: Box::new(Cell::Nil),
-                }
+            bst == BST::Cons {
+                val: 2,
+                left: Box::new(BST::Nil),
+                right: Box::new(BST::Nil),
+            }
         );
         bst.insert(1);
         assert!(
-            bst.head
-                == Cell::Cons {
-                    val: 2,
-                    left: Box::new(Cell::new_with(1)),
-                    right: Box::new(Cell::Nil),
-                }
+            bst == BST::Cons {
+                val: 2,
+                left: Box::new(BST::new_with(1)),
+                right: Box::new(BST::Nil),
+            }
+        );
+    }
+    #[test]
+    fn can_search() {
+        let mut bst = BST::new_with(3);
+        bst.insert(5);
+        bst.insert(6);
+        bst.insert(7);
+
+        let node = bst.search(&8);
+        assert!(node == &BST::Nil);
+
+        let node = bst.search(&7);
+        assert!(node == &BST::new_with(7));
+
+        let node = bst.search(&6);
+        assert!(
+            node == &BST::Cons {
+                val: 6,
+                left: Box::new(BST::Nil),
+                right: Box::new(BST::new_with(7))
+            }
         );
     }
 }
